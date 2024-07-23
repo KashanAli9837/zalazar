9737a9758bfa523224b1de5567934158c5b8b9c6
Kashan Ali <137059086+KashanAli9837@users.noreply.github.com>
2024-07-22 18:36:04 -0700

    home

---
+++ logics/hero.js
@@ -0,0 +1,31 @@
+const menu_icon = document.querySelector(".menu_icon");
+const cross_icon = document.querySelector(".cross_icon");
+const nav = document.querySelector("nav");
+
+menu_icon.addEventListener("click", () => {
+  if (nav.style.left === "0px") {
+    nav.style.left = "-350px";
+  } else {
+    nav.style.left = "0px";
+  }
+  console.log(nav.style.left);
+});
+
+cross_icon.addEventListener("click", () => {
+  nav.style.left = "-350px";
+});
+
+// TypeWrite Animation
+const element = document.querySelector(".typingTextContainer");
+const txt = "Engineering";
+const speed = 70;
+let i = 0;
+
+function typeWriter() {
+  if (i < txt.length) {
+    element.innerHTML += txt[i];
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
