f7ae5114d7b2449e4bdd8a8e7074ff7eef93a0a2
Kashan Ali <137059086+KashanAli9837@users.noreply.github.com>
2024-07-23 13:23:12 -0700

    1

--- logics/hero.js
+++ logics/hero.js
@@ -1,18 +1,3 @@
-const menu_icon = document.querySelector(".menu_icon");
-const cross_icon = document.querySelector(".cross_icon");
-const nav = document.querySelector("nav");
-
-menu_icon.addEventListener("click", () => {
-  if (nav.style.left === "0px") {
-    nav.style.left = "-350px";
-  } else {
-    nav.style.left = "0px";
-  }
-});
-
-cross_icon.addEventListener("click", () => {
-  nav.style.left = "-350px";
-});
 
 // TypeWrite Animation
 const element = document.querySelector(".typingTextContainer");
---
+++ logics/menu.js
@@ -0,0 +1,15 @@
+const menu_icon = document.querySelector(".menu_icon");
+const cross_icon = document.querySelector(".cross_icon");
+const nav = document.querySelector("nav");
+
+menu_icon.addEventListener("click", () => {
+  if (nav.style.left === "0px") {
+    nav.style.left = "-350px";
+  } else {
+    nav.style.left = "0px";
+  }
+});
+
+cross_icon.addEventListener("click", () => {
+  nav.style.left = "-350px";
+});
