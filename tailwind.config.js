29d830bc83438c7d2b23b79f65c08f263e520692
Kashan Ali <137059086+KashanAli9837@users.noreply.github.com>
2024-07-23 21:35:46 -0700

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,20 @@ module.exports = {
           "50%": { opacity: "1" },
           "100%": { opacity: "1" },
         },
+        'bounce-right': {
+          "0%, 100%": {
+            transform: "translateX(-25%)",
+            animationTimingFunction: "cubic-bezier(0.8,0,1,1)",
+          },
+          "50%": { 
+            transform: "none",
+            animationTimingFunction: "cubic-bezier(0,0,0.2,1)",
+           },
+        },
       },
       animation: {
-        "blink": "blink 1s ease-out infinite",
+        blink: "blink 1s ease-out infinite",
+        "bounce-right": "bounce-right 1s infinite",
       },
     },
   },
