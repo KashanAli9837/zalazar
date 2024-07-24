975c7b7055fdd76eddea376a9049a637def982ad
Kashan Ali <137059086+KashanAli9837@users.noreply.github.com>
2024-07-24 11:19:08 -0700

    final

--- logics/menu.js
+++ logics/menu.js
@@ -1,15 +1,26 @@
 const menu_icon = document.querySelector(".menu_icon");
 const cross_icon = document.querySelector(".cross_icon");
 const nav = document.querySelector("nav");
+const overlay = document.querySelector(".overlay");
 
 menu_icon.addEventListener("click", () => {
+  toggleMenu();
+});
+
+cross_icon.addEventListener("click", () => {
+  nav.style.left = "-400px";
+  overlay.classList.toggle('hidden');
+});
+
+overlay.addEventListener("click", () => {
+  toggleMenu();
+})
+
+function toggleMenu() {
   if (nav.style.left === "0px") {
     nav.style.left = "-400px";
   } else {
     nav.style.left = "0px";
   }
-});
-
-cross_icon.addEventListener("click", () => {
-  nav.style.left = "-400px";
-});
+  overlay.classList.toggle('hidden');
+}
